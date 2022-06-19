b63481acf670c7566532d9a26fd7c2e1ac6cf2a2
Julia Napora <jula.napa.111115@gmail.com>
2022-06-19 21:43:51 +0200

    deploy

---
+++ build/precache-manifest.71648d0d6362a050a09c2dc4c4a78057.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16fe91653a19a9dc80f5ffbfe6a36bf2",
+    "url": "/game_of_life-/index.html"
+  },
+  {
+    "revision": "dc5f44f94a9e6b3f7908",
+    "url": "/game_of_life-/static/js/2.87d45293.chunk.js"
+  },
+  {
+    "revision": "03c58bd081515bf24708",
+    "url": "/game_of_life-/static/js/main.a6270e1b.chunk.js"
+  },
+  {
+    "revision": "2d8350db03de8787fd52",
+    "url": "/game_of_life-/static/js/runtime-main.efcc62d0.js"
+  }
+]);
